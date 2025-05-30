6ad0618e5cd824887392ccb8f2e066919d8ff965
DS7 <p.danielsalvador@gmail.com>
2025-05-30 14:35:56 -0300

    Locadora de carros
    Criação de um menu de opções de carros.

---
+++ locadora_de_carros.py
@@ -0,0 +1,30 @@
+def mostrar_linha():
+
+    print('-' * 50)
+
+# Exemplo de uso da função mostrar_linha
+mostrar_linha()
+
+print("Aluguel de Carro com a maior frota do Brasil | Localiza")
+
+mostrar_linha()
+
+nome = input('Digite seu nome:')
+print(f'Olá,{nome}! Estamos felizes em tê-lo conosco.')
+
+mostrar_linha()
+
+carro = input('Digite qual carro deseja alugar: (1-BMW | 2-Mustang | 3-HB20 | 4-Fusca | 5-Civic | 0-sair):')
+
+if carro == 'BMW':
+    print(f'Sr(a). {nome}, você escolheu um ótimo carro! da marca {carro}')
+elif carro == 'Mustang':
+    print(f'Sr(a).{nome} você escolheu um belo carro para sair e tirar aquela onda com seu {carro} ')
+elif carro == 'HB20':
+    print(f'Olá Sr(a). {nome}, Você escolheu um belo carro! Da marca {carro}')
+elif carro == 'Fusca':
+    print(f'Ótima escolha de carro Sr(a). {nome}, um exelente {carro}!')
+elif carro == 'Civic':
+    print(f'Boa escolha do seu {carro}! Sr(a).{nome}')
+else:
+    print(f'Que pena que voce não escolheu, mas tenha um ótimo dia! Sr(a).{nome}')
